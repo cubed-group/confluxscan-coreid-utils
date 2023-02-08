7001f57f94e793dc62bb3c91d3bee287e06d9b8a
tangxuezhi <tangxuezhi@conflux-chain.org>
2023-02-08 10:49:10 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -24,13 +24,21 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ["react", "react-dom", "js-conflux-sdk"],
+      external: [
+        "react",
+        "react-dom",
+        "js-conflux-sdk",
+        "bignumber.js",
+        "buffer",
+      ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           react: "react",
           "react-dom": "react-dom",
           "js-conflux-sdk": "js-conflux-sdk",
+          "bignumber.js": "bignumber.js",
+          buffer: "buffer",
         },
       },
     },
